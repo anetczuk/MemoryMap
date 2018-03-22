1e4f98a094cbffbe0496a2bec07915a094910047
anetczuk <anetczuk@o2.pl>
2018-03-22 23:43:29 +0100

    Update and copy script

--- src/clanganalyzer.sh
+++ src/clanganalyzer.sh
@@ -7,10 +7,10 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
-if [ "$(pwd)" == "$SCRIPT_DIR" ]; then
-    echo "Building inside source code is forbidden. Change current working directory"
-    exit 1
-fi
+##if [ "$(pwd)" == "$SCRIPT_DIR" ]; then
+##    echo "Building inside source code is forbidden. Change current working directory"
+##    exit 1
+##fi
 
 
 scan-build cmake $SCRIPT_DIR
